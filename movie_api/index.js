e2917e575c19de5967485bcf599f1dae44c3d614
Mimi <76797616+rumpeltin@users.noreply.github.com>
2022-06-07 20:54:30 +0100

    impl. express, morgan & error-handling middleware

--- movie_api/index.js
+++ movie_api/index.js
@@ -1,13 +1,74 @@
-const express = require("express");
+// imported modules
+const express = require('express');
+  morgan = require('morgan');
+
+
 const app = express();
 
-const http = require("http");
+// middleware
+app.use(morgan('common'));
+app.use(express.static('public'));
 
-http
-  .createServer((request, response) => {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("Welcome to my book club!\n");
-  })
-  .listen(8080);
+// movie object
+let topMovies = [
+  {
+    title: 'PofC: On Stranger Tides',
+    director: 'Rob Marshall'
+  },
+  {
+    title: 'Charlie and the Chocolate Factory',
+    director: 'Tim Burton'
+  },
+  {
+    title: 'PofC: Dead Man\'s Chest',
+    director: 'Gore Verbinski'
+  },
+  {
+    title: 'Platoon',
+    director: 'Oliver Stone '
+  },
+  {
+    title: 'Donnie Brasco',
+    director: 'Mike Newell'
+  },
+  {
+    title: 'Alice in Wonderland ',
+    director: 'Tim Burton'
+  },
+  {
+    title: 'Public Enemies',
+    director: 'Michael Mann'
+  },
+  {
+    title: 'A Nightmare on Elm Street',
+    director: 'Wes Craven'
+  },
+  {
+    title: 'Fear and Loathing in Las Vegas',
+    director: 'Terry Gilliam'
+  },
+  {
+    title: 'Cry-Baby',
+    director: 'John Waters'
+  }
+];
 
-console.log("My first Node test server is running on Port 8080.");
+// GET requests
+app.get('/', (req, res) => {
+  res.send('Welcome to DeppFlix!');
+});
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
+
+// error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// listen for requests
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
